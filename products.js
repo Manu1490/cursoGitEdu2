1791c3ebc5bf9cef881a2d1d940ca05dbce2aff3
Manuel Sanz <angelmsanzr@gmail.com>
2025-03-18 19:53:39 -0300

    Agrego vista de productos

---
+++ products.js
@@ -0,0 +1,16 @@
+import react from 'react'
+import protoTyoes from 'prop-types'
+
+export const productos = props => {
+    return (
+        <div
+        >
+        </div>
+    )
+}
+
+productos.protoTypes = {
+
+}
+
+export default products
